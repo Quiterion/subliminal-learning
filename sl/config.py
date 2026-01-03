38edf8793d2be554ff6f872465e0ec520bdd385b
Jamie <jden4399@uni.sydney.edu.au>
2026-01-03 22:46:57 +1100

    fix: bump default VLLM_MAX_LORA_RANK to 16

--- sl/config.py
+++ sl/config.py
@@ -8,5 +8,5 @@ HF_TOKEN = os.getenv("HF_TOKEN", "")
 HF_USER_ID = os.getenv("HF_USER_ID", "")
 
 VLLM_N_GPUS = int(os.getenv("VLLM_N_GPUS", 1))
-VLLM_MAX_LORA_RANK = int(os.getenv("VLLM_MAX_LORA_RANK", 8))
+VLLM_MAX_LORA_RANK = int(os.getenv("VLLM_MAX_LORA_RANK", 16))
 VLLM_MAX_NUM_SEQS = int(os.getenv("VLLM_MAX_NUM_SEQS", 512))
